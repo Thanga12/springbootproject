ddcfa07225ce1398298c4b6551a1d04fb8ee6bbf
AKASH ABH <AKASH ABH@DESKTOP-67DDHO8>
2024-02-21 12:04:09 +0530

    This is my springboot project

---
+++ PlacementService-master/src/main/java/com/Thangameena/PlacementService/PlacementRepository.java
@@ -0,0 +1,7 @@
+package com.Thangameena.PlacementService;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlacementRepository extends JpaRepository<Placement, Integer>{
+
+}
